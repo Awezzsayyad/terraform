f9996227ed690322ff85e7131d9ebe301b6adf8e
Awezzsayyad <awezz77@outlook.com>
2024-12-05 17:10:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,9 @@
 resource "local_file" "my_pet" {
-filename = "pets.txt"
-content = "I love pets!"
+filename = "/pets.txt"
+content = "My cat name is CAT"
+}
+resource "random_pet" "petname" {
+prefix = "MR"
+separator = "."
+length = "1"
 }
