22e61dc08ad87aae98af68152fc836d8f30397ca
Awezzsayyad <awezz77@outlook.com>
2024-12-05 14:53:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,34 +1,4 @@
-pipeline {
-    agent any
-    stages {
-        stage('Clone') {
-            steps {
-                git branch: 'main', url: 'https://github.com/Awezzsayyad/terraform.git'
-            }
-        }
-        stage('Initialize Terraform') {
-            steps {
-                sh 'terraform init'
-            }
-        }
-        stage('Execute Terraform Apply') {
-            steps {
-                sh 'terraform apply -auto-approve'
-            }
-        }
-    }
+resource "local_file" "my_pet" {
+filename = "pets.txt"
+content = "I love pets!"
 }
-
-React
-
-Reply
-
-
-
-
-
-
-
-
-
-
