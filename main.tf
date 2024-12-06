79b02e5892ddb8887b4b3c4a563f4fccd719abe6
Awezzsayyad <awezz77@outlook.com>
2024-12-06 15:52:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,3 +7,17 @@ prefix = "MR"
 separator = "."
 length = "1"
 }
+resource "local_file" "my-pet" {
+filename = var.filename[count.index]
+content = "I love cats!"
+count = 3
+}
+
+
+variable "filename" {
+default = [
+"pets.txt",
+"cats.txt",
+"dogs.txt"
+]
+}
