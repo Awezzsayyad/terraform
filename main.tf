637318e93b03f425e7e424afaacc61cac671c77f
Awezzsayyad <awezz77@outlook.com>
2024-12-06 15:27:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,17 +20,3 @@ variable "prefix" {
 }
 
 
-resource "local_file" "my-pet" {
-filename = var.filename[count.index]
-content = "I love cats!"
-count = 3
-}
-
-
-variable "filename" {
-default = [
-"pets.txt",
-"cats.txt",
-"dogs.txt"
-]
-}
